3f8b47056920e036d66039012c8ce14b99b50fd6
unknown <vij.kr.rana@gmail.com>
2019-07-18 20:37:42 +0530

    first commit

---
+++ src/main/java/com/innvn/innvnpetclinic/InnvnPetclinicApplication.java
@@ -0,0 +1,13 @@
+package com.innvn.innvnpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnvnPetclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnvnPetclinicApplication.class, args);
+	}
+
+}
